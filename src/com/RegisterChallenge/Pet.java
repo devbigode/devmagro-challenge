d840a02862b6b031d676e4d6c9cc86105275e38b
Dev <guilherme.ads@proton.me>
2025-04-08 12:09:00 -0300

    Implementação dos getters/setters + toString()

--- src/com/RegisterChallenge/Pet.java
+++ src/com/RegisterChallenge/Pet.java
@@ -1,13 +1,85 @@
 package src.com.RegisterChallenge;
 
-public class Pet {
+public class Pet extends Address{
 
     private String name;
     private String lastName;
     private double age;
     private double weight;
+    private String breed;
     private Type type;
     private Sex sex;
-    private Address address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Type getType() {
+
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", breed='" + breed + '\'' +
+                ", type=" + type +
+                ", sex=" + sex +
+                ", number=" + getNumber() +
+                ", city='" + getCity() + '\'' +
+                ", street='" + getStreet() + '\'' +
+                '}';
+    }
 }
